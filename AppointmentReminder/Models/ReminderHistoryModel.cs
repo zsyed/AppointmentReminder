e515d9cdfa580027d3d96d2073bbf050649d128d
zsyed <zulfiqarsyed@yahoo.com>
2014-10-09 21:09:24 -0700

    Adding Reminder History

---
+++ AppointmentReminder/Models/ReminderHistoryModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppointmentReminder.Models
+{
+	public class ReminderHistoryModel
+	{
+		public int Id { get; set; }
+		public DateTime ReminderDateTime { get; set; }
+		public string Message { get; set; }
+		public int ContactId { get; set; }
+		public string ContactName { get; set; }
+		public bool EmailSent { get; set; }
+		public bool SMSSent { get; set; }
+		public DateTime MessageSentDateTime { get; set; }
+	}
+}
