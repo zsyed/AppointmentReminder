1998218ef62c7bcb3d533882e6138ac949d4dc46
zsyed <zulfiqarsyed@yahoo.com>
2014-10-23 16:50:56 -0700

    bootstrapped the edit profile submit button.

---
+++ AppointmentReminder/obj/Release/Package/PackageTmp/js/profile-index.js
@@ -0,0 +1,44 @@
+﻿(function () {
+
+	var app = angular.module("ProfileModule", []);
+
+	var ProfileIndexController = function($scope, $http) {
+		$http.get("/api/ProfileWeb")
+			.then(function(response) {
+				$scope.profile = response.data;
+			});
+
+		var s = $scope.profile;
+	};
+
+
+	var ProfileEditController = function ($scope, $http) {
+
+		$http.get("/api/ProfileWeb")
+			.then(function(response) {
+				$scope.profile = response.data;
+			});
+
+		$scope.saveEditProfile = function () {
+			$http.put("/api/ProfileWeb", $scope.profile)
+				.then(function(response) {
+					$scope.profile = response.data;
+				});
+		};
+	};
+
+	var ProfileCreateController = function ($scope, $http) {
+		$scope.saveCreateProfile = function () {
+			$http.post("/api/ProfileWeb", $scope.profile)
+				.then(function(response) {
+					$scope.profile = response.data;
+				});
+		};
+
+	};
+
+	app.controller("ProfileIndexController", ["$scope", "$http", ProfileIndexController]);
+	app.controller("ProfileEditController", ["$scope", "$http", ProfileEditController]);
+	app.controller("ProfileCreateController", ["$scope", "$http", ProfileCreateController]);
+
+}());
